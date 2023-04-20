2d447575d60c48bba18d3cd765a40331d2662bd7
Mikita <anykey2solo@gmail.com>
2023-04-20 21:00:48 +0300

    refactor(basic-3.1,3.2,3.3): update selector names buttons in js

--- app.js
+++ app.js
@@ -41,9 +41,10 @@ var createNewTaskElement=function(taskString){
     editInput.className="todo-list__input-task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="todo-list__button todo-list__button_edit";
+    
 
-    deleteButton.className="delete";
+    deleteButton.className="todo-list__button todo-list__button_delete";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -84,7 +85,7 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
+    var editBtn=listItem.querySelector(".todo-list__button_edit");
     var containsClass=listItem.classList.contains("editMode");
     //If class of the parent is .editmode
     if(containsClass){
@@ -156,8 +157,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var editButton=taskListItem.querySelector(".todo-list__button_edit");
+    var deleteButton=taskListItem.querySelector(".todo-list__button_delete");
 
 
     //Bind editTask to edit button.
